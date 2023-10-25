3688db476dc514d47870cd7535a9edc02677cc57
astral0525 <astral0525@gmail.com>
2023-10-25 15:38:05 +0900

    ëª1단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,2 +1,24 @@
-package com.ll;public class App {
+package com.ll;
+
+import java.util.Scanner;
+
+class App {
+    void run() {
+        System.out.println("==명언 앱==");
+
+        while(true) {
+            System.out.print("명령) ");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+            if(cmd.equals("종료")){
+                break;
+            }
+            System.out.printf("입력하신명령 : %s\n", cmd);
+        }
+
+
+
+    }
 }
